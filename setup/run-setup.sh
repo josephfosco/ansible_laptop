4d35239038b12e794e37e62ea611642d1ac3f45d
Joseph Fsoco <joseph@josephfosco.com>
2017-08-13 19:55:43 -0500

    Initial commit

---
+++ setup/run-setup.sh
@@ -0,0 +1,6 @@
+# cd ~/py/ansible/
+# source ~/py/ansible/ansible/hacking/env-setup
+# source ~/py/ansible/env/bin/activate
+# cd ansible
+# ansible-playbook ../setup/setup.yml -K
+~/sys/ansible/env/bin/ansible-playbook setup.yml -K
